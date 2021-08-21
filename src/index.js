cc879dffd206500cbf418c86d66ba16635ca4c5a
leosouza-dev <leonardo.89@uol.com.br>
2021-08-21 08:51:43 -0300

    Criando saque na conta

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { v4: uuidV4 } = require("uuid");
-
 const app = express();
+
 app.use(express.json());
 
 // BD fake
@@ -17,6 +17,15 @@ function verifyIfExistsAccountCPF(request, response, next){
     request.customer = customer;
 
     return next();
+}
+
+function getBalance(statement){
+    const balance = statement.reduce((acc, operation) => {
+        if(operation.type === "credit") return acc + operation.amount;
+        if(operation.type === "debit") return acc - operation.amount;
+    }, 0);
+
+    return balance;
 }
 
 app.post("/account", (request, response) => {
@@ -55,6 +64,26 @@ app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
     customer.statement.push(statementOperation);
 
     return response.status(201).send();
+});
+
+app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
+    const { description, amount} = request.body;
+    const { customer } = request;
+    const balance = getBalance(customer.statement);
+
+    if(balance < amount) 
+        return response.status(400).json({error: "Saldo Insuficiente"});
+    
+        const statementOperation = {
+            description,
+            amount,
+            created_at: new Date(),
+            type: "debit"
+        };
+    
+        customer.statement.push(statementOperation);
+    
+        return response.status(201).send();
 });
 
 app.listen(3333);
